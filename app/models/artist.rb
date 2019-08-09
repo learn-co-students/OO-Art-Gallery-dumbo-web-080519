ab57819fc34c1598ec143002dd385bb71838d0e5
github username <github email address>
2019-08-09 17:22:39 -0400

    art-gallery update

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,69 @@
+require 'pry'
+
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end 
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end 
+  end 
+
+  def galleries
+    self.paintings.map do |painting|
+      painting.gallery 
+    end 
+  end 
+
+  def cities 
+   self.galleries.map do |gallery|
+      gallery.city
+    end 
+  end 
+
+  def self.total_experience #.inject method
+    total_yrs = 0
+    self.all.each do |artist|
+      total_yrs += artist.years_experience
+    end
+    total_yrs
+    # (self.all.map {|artist| artist.years_experience }).sum 
+
+    # total = self.all.map do |artist|
+    #   artist.years_experience
+    # end 
+    # total.sum
+  end
+
+  def self.most_prolific
+    most_prolific = nil
+    highest_artist = 0.0
+    self.all.each do |artist|
+      average = artist.paintings.size.to_f/artist.years_experience.to_f
+      if average > highest_artist 
+        highest_artist = average 
+        most_prolific = artist
+      end 
+    end 
+    most_prolific
+
+  end 
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end 
+
 end
+
--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,19 @@ require 'pry'
 require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
+
+brian = Artist.new("Brian", 1)
+kim = Artist.new("Kim", 3)
+
+moma = Gallery.new("MOMA","New York")
+museum = Gallery.new("Museum", "DC")
+anime = Painting.new("pikachu", 100, brian, moma)
+anime1 = Painting.new("pikachu1", 101, brian, moma)
+spongebob = Painting.new("squarepants", 50, kim, museum)
+
+
+
+
+binding.pry
+
+0
