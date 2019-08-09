d939c3a95d3fc4ef7075829a6092fb7413c33e81
Josh Nho <joshnho@gmail.com>
2019-08-09 17:05:46 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,56 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    Gallery.all.select do |gallery|
+      gallery.artist == self
+    end
+  end
+
+  def cities
+    paintings.map {|painting| 
+    painting.gallery.city
+    } 
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  def self.total_experience
+    (@@all.map {|artist| artist.years_experience}).sum
+  end
+
+  def self.most_prolific
+    legend = nil
+    highest_average = 0.00
+    @@all.each do |artist| 
+      average = artist.paintings.size.to_f / artist.years_experience.to_f
+      if average > highest_average
+        legend = artist
+        highest_average = average
+      end
+    end
+    legend
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,20 @@
 require_relative '../config/environment.rb'
 
 
+michelle = Artist.new("michelle", 50)
+joshua = Artist.new("joshua", 25)
+avi = Artist.new("avi", 1000)
+
+gallery1 = Gallery.new("MSG", "Newyork")
+gallery2 = Gallery.new("salt", "Maine")
+gallery3 = Gallery.new("Moon", "California")
+
+painting1 = Painting.new("Test", 101,michelle, gallery2)
+painting2 = Painting.new("Test", 110, michelle, gallery1)
+painting3 = Painting.new("Test", 100, joshua, gallery1)
+painting4 = Painting.new("Test", 500, joshua, gallery2)
 
 
-binding.pry
+Pry.start
 
 puts "Bob Ross rules."
